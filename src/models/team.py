c1602d52b70f276a8c78c115951fffb609368ae0
joseantonio.sanchez <joseantonio.sanchez@airenetworks.es>
2026-04-13 12:31:55 +0200

    clean proyect

--- src/models/team.py
+++
@@ -1,9 +0,0 @@
-"""Modelo de datos para equipos."""
-from dataclasses import dataclass
-from typing import Optional
-
-@dataclass
-class Team:
-    id: int
-    name: str
-    short_name: Optional[str] = None
