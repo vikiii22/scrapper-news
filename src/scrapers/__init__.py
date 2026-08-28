06a67061ff51f54507751c3f2851850dd8224865
Jose Antonio Sanchez <joseantonio.sanchez@airenetworks.es>
2026-08-28 13:54:36 +0200

    test

--- src/scrapers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .sofascore import SofascoreScraper
-from .news_scraper import NewsScraper
-from .quiniela_html import QuinielaHtmlParser
-
-__all__ = ["SofascoreScraper", "NewsScraper", "QuinielaHtmlParser"]
